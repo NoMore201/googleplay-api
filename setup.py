8b5eec3322c67d6b40c0366a7d73331af9845ded
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2017-09-28 10:55:00 +0200

    Prepare for v0.1.1
    Signed-off-by: Domenico Iezzi <domenico.iezzi.201@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='googleplay-api',
-    version='0.1.0',
+    version='0.1.1',
     description='Unofficial python3 api for google play',
     url='https://github.com/NoMore201/googleplay-api',
     author='NoMore201',
