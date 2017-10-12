e1cc147db466515b503d44f8256597c350a6df8d
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2017-10-12 19:31:22 +0200

    Change pycrytpo dependecy with pycryptodome
    Since pycrypto is a dead project, it's better to use an up-to-date
    fork with the same API.
    
    Signed-off-by: Domenico Iezzi <domenico.iezzi.201@gmail.com>

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 from setuptools import setup
 
-setup(
-    name='gpapi',
-    version='0.2.1',
-    description='Unofficial python api for google play',
-    url='https://github.com/NoMore201/googleplay-api',
-    author='NoMore201',
-    author_email='domenico.iezzi.201@gmail.com',
-    license='MIT',
-    packages=['gpapi'],
-    package_data={
-        'gpapi': ['device.properties'],
-    },
-    install_requires=['pycrypto',
-                     'protobuf',
-                     'clint',
-                     'requests'],
-)
+setup(name='gpapi',
+      version='0.2.1',
+      description='Unofficial python api for google play',
+      url='https://github.com/NoMore201/googleplay-api',
+      author='NoMore201',
+      author_email='domenico.iezzi.201@gmail.com',
+      license='MIT',
+      packages=['gpapi'],
+      package_data={'gpapi': ['device.properties']},
+      install_requires=['pycryptodome',
+                        'protobuf',
+                        'clint',
+                        'requests'])
