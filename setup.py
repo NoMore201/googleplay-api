5013dbe5ab73c45ebdeabaae55d3c327e94235c0
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2017-12-11 19:30:30 +0100

    Update setup.py to version 0.3.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gpapi',
-      version='0.3.1',
+      version='0.3.2',
       description='Unofficial python api for google play',
       url='https://github.com/NoMore201/googleplay-api',
       author='NoMore201',
